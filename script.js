c1c593644c99f27d0e1f8b5c3da9e915d8ddff48
Bastien Delseny <thebigpatate@gmail.com>
2018-10-10 23:17:09 +0200

    Toggle menu for small screen and change colors

---
+++ script.js
@@ -0,0 +1,17 @@
+// Refresh web page for DEV
+// var timeout = 10000;
+// setTimeout(() => {
+//     document.location.reload(true);
+// }, timeout);
+
+/**
+ * Toggle navigation bar
+ * @author BDY
+ */
+var toggleNavigationBar = function () {
+    const mainNav = document.getElementById('js-menu');
+    const toggleButton = document.getElementById('js-nav-toggle');
+    toggleButton.addEventListener('click', () => mainNav.classList.toggle('active'));
+};
+
+toggleNavigationBar();
